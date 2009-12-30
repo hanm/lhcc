89300d3907846020b9b50d1de88090ed8cb86191
lianghancn <lianghancn@b2a9d1c8-15c3-11de-babb-ddc7aea8d8a1>
2009-12-30 10:25:12 +0000

    more statement ast work. basic infrastructure ready

--- trunk/ast.c
+++ trunk/ast.c
@@ -79,13 +79,16 @@ This would require hundreds of recursive structs in C or classes in Java.
 An alternative method is to use just one generic tree structure to capture all possible tree structures. 
 */
 
-#define ALLOCATE_GENERIC_AST  t_ast_exp* exp; \
+#define ALLOCATE_GENERIC_AST_EXP  t_ast_exp* exp; \
 	CALLOC(exp, PERM)
+
+#define ALLOCATE_GENERIC_AST_STMT t_ast_stmt* stmt; \
+	CALLOC(stmt, PERM)
 
 
 t_ast_exp* make_ast_id_exp(char* name)
 {
-	ALLOCATE_GENERIC_AST;
+	ALLOCATE_GENERIC_AST_EXP;
 
 	assert(name);
 
@@ -97,7 +100,7 @@ t_ast_exp* make_ast_id_exp(char* name)
 
 t_ast_exp* make_ast_const_exp(t_ast_exp_val val, t_ast_exp_kind kind)
 {
-	ALLOCATE_GENERIC_AST;
+	ALLOCATE_GENERIC_AST_EXP;
 
     assert(kind == AST_EXP_CONST_FLOAT_KIND ||
               kind == AST_EXP_CONST_DOUBLE_KIND ||
@@ -119,7 +122,7 @@ t_ast_exp* make_ast_const_exp(t_ast_exp_val val, t_ast_exp_kind kind)
 
 t_ast_exp* make_ast_subscript_exp(t_ast_exp* main, t_ast_exp* index)
 {
-	ALLOCATE_GENERIC_AST;
+	ALLOCATE_GENERIC_AST_EXP;
 
 	assert(main && index);
 
@@ -132,7 +135,7 @@ t_ast_exp* make_ast_subscript_exp(t_ast_exp* main, t_ast_exp* index)
 
 t_ast_exp* make_ast_call_exp(t_ast_exp* func, t_ast_list args)
 {
-	ALLOCATE_GENERIC_AST;
+	ALLOCATE_GENERIC_AST_EXP;
 
 	assert(func);
 
@@ -145,7 +148,7 @@ t_ast_exp* make_ast_call_exp(t_ast_exp* func, t_ast_list args)
 
 t_ast_exp* make_ast_indir_exp(t_ast_exp* expression, t_ast_exp_op op, char* id)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
 
     assert(expression && id);
     assert(op == AST_OP_PTR || op == AST_OP_DOT);
@@ -160,7 +163,7 @@ t_ast_exp* make_ast_indir_exp(t_ast_exp* expression, t_ast_exp_op op, char* id)
 
 t_ast_exp* make_ast_postop_exp(t_ast_exp* expression, t_ast_exp_op op)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
 
 	assert(expression);
     assert(op == AST_OP_INC || op == AST_OP_DEC);
@@ -174,7 +177,7 @@ t_ast_exp* make_ast_postop_exp(t_ast_exp* expression, t_ast_exp_op op)
 
 t_ast_exp* make_ast_unary_exp(t_ast_exp* expression, t_ast_exp_op op)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
 
     assert(expression);
     assert(op == AST_OP_ADDR ||
@@ -195,7 +198,7 @@ t_ast_exp* make_ast_unary_exp(t_ast_exp* expression, t_ast_exp_op op)
 
 t_ast_exp* make_ast_cast_exp(t_ast_exp* type, t_ast_exp* expression)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
     
     assert(type && expression);
 
@@ -208,7 +211,7 @@ t_ast_exp* make_ast_cast_exp(t_ast_exp* type, t_ast_exp* expression)
 
 t_ast_exp* make_ast_sizeof_exp(t_ast_exp* type, t_ast_exp* expression)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
 
     assert(type || exp); 
 
@@ -221,7 +224,7 @@ t_ast_exp* make_ast_sizeof_exp(t_ast_exp* type, t_ast_exp* expression)
 
 t_ast_exp* make_ast_binary_exp(t_ast_exp* left, t_ast_exp_op op, t_ast_exp* right)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
 
     assert(left && right);
 
@@ -235,7 +238,7 @@ t_ast_exp* make_ast_binary_exp(t_ast_exp* left, t_ast_exp_op op, t_ast_exp* righ
 
 t_ast_exp* make_ast_conditional_exp(t_ast_exp* cond_exp, t_ast_exp* true_exp, t_ast_exp* false_exp)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
 
     assert(cond_exp);
     assert(!(!true_exp && !false_exp));
@@ -250,7 +253,7 @@ t_ast_exp* make_ast_conditional_exp(t_ast_exp* cond_exp, t_ast_exp* true_exp, t_
 
 t_ast_exp* make_ast_assignment_exp(t_ast_exp* cond_exp, t_ast_exp_op op, t_ast_exp* assign_exp)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
 
     assert(cond_exp);
 
@@ -264,7 +267,7 @@ t_ast_exp* make_ast_assignment_exp(t_ast_exp* cond_exp, t_ast_exp_op op, t_ast_e
 
 t_ast_exp* make_ast_comma_exp(t_ast_exp* comma_exp, t_ast_exp* assign_exp)
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
 
     assert(assign_exp);
 
@@ -280,7 +283,7 @@ t_ast_exp* make_ast_comma_exp(t_ast_exp* comma_exp, t_ast_exp* assign_exp)
 
 t_ast_exp* make_ast_typename_exp()
 {
-    ALLOCATE_GENERIC_AST;
+    ALLOCATE_GENERIC_AST_EXP;
     exp->kind = AST_EXP_GENERIC_EXP_KIND;
 
     return exp;
@@ -288,8 +291,147 @@ t_ast_exp* make_ast_typename_exp()
 
 t_ast_exp* make_ast_generic_exp()
 {
-	ALLOCATE_GENERIC_AST;
+	ALLOCATE_GENERIC_AST_EXP;
 	exp->kind = AST_EXP_GENERIC_EXP_KIND;
 
 	return exp;
+}
+
+t_ast_stmt* make_ast_label_stmt(char* label_name, t_ast_stmt* label_stmt)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(label_name && label_stmt);
+
+	stmt->kind = AST_STMT_LABEL_KIND;
+	stmt->u.ast_label_stmt.label_name = label_name;
+	stmt->u.ast_label_stmt.stmt = label_stmt;
+	
+	return stmt;
+}
+
+t_ast_stmt* make_ast_expression_stmt(t_ast_exp* exp)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(exp);
+
+	stmt->kind = AST_STMT_EXPRESSION_KIND;
+	stmt->u.ast_expression_stmt.exp = exp;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_if_stmt(t_ast_exp* test_exp, t_ast_stmt* then_stmt, t_ast_stmt* else_stmt)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(test_exp && then_stmt);
+
+	stmt->kind = AST_STMT_IF_KIND;
+	stmt->u.ast_if_stmt.test_exp = test_exp;
+	stmt->u.ast_if_stmt.then_stmt = then_stmt;
+	stmt->u.ast_if_stmt.else_stmt = else_stmt;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_switch_stmt(t_ast_exp* test_exp, t_ast_stmt* switch_stmt)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(test_exp && switch_stmt);
+
+	stmt->kind = AST_STMT_SWITCH_KIND;
+	stmt->u.ast_switch_stmt.test_exp = test_exp;
+	stmt->u.ast_switch_stmt.stmt = switch_stmt;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_do_stmt(t_ast_stmt* body_stmt, t_ast_exp* test_exp)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(body_stmt && test_exp);
+
+	stmt->kind = AST_STMT_DO_KIND;
+	stmt->u.ast_do_stmt.body_stmt = body_stmt;
+	stmt->u.ast_do_stmt.test_exp = test_exp;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_while_stmt(t_ast_exp* test_exp, t_ast_stmt* body_stmt)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(test_exp && body_stmt);
+
+	stmt->kind = AST_STMT_WHILE_KIND;
+	stmt->u.ast_while_stmt.body_stmt = body_stmt;
+	stmt->u.ast_while_stmt.test_exp = test_exp;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_for_stmt(t_ast_stmt* init_exp_stmt, t_ast_stmt* test_exp_stmt, t_ast_exp* post_test_exp, t_ast_stmt* body_stmt)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(init_exp_stmt && test_exp_stmt && body_stmt);
+
+	stmt->kind = AST_STMT_FOR_KIND;
+	stmt->u.ast_for_stmt.init_exp_stmt = init_exp_stmt;
+	stmt->u.ast_for_stmt.test_exp_stmt = test_exp_stmt;
+	stmt->u.ast_for_stmt.post_test_exp = post_test_exp;
+	stmt->u.ast_for_stmt.body_stmt = body_stmt;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_goto_stmt(char* label_name)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(label_name);
+
+	stmt->kind = AST_STMT_GOTO_KIND;
+	stmt->u.ast_goto_stmt.label_name = label_name;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_continue_stmt(t_ast_stmt* target)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(target);
+	
+	stmt->kind = AST_STMT_CONTINUE__KIND;
+	stmt->u.ast_continue_stmt.target = target;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_break_stmt(t_ast_stmt* target)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	assert(target);
+
+	stmt->kind = AST_STMT_BREAK_KIND;
+	stmt->u.ast_break_stmt.target = target;
+
+	return stmt;
+}
+
+t_ast_stmt* make_ast_return_stmt(t_ast_exp* return_exp)
+{
+	ALLOCATE_GENERIC_AST_STMT;
+
+	stmt->kind = AST_STMT_RETURN_KIND;
+	stmt->u.ast_return_stmt.exp = return_exp;
+
+	return stmt;
 }
