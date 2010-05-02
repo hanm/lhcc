95de3f3939523fd31fc07ad93c06d75a11f59c7a
lianghancn <lianghancn@b2a9d1c8-15c3-11de-babb-ddc7aea8d8a1>
2010-05-02 09:09:19 +0000

    more ssc for expressions

--- trunk/ssc-exp.c
+++ trunk/ssc-exp.c
@@ -29,6 +29,14 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 #include "ast.h"
 
+/* Define NULL pointer value */
+#ifndef NULL
+#ifdef __cplusplus
+#define NULL    0
+#else
+#define NULL    ((void *)0)
+#endif
+#endif
 
 static t_ast_exp* scc_primary_expression(t_ast_exp* exp)
 {
@@ -50,67 +58,205 @@ static t_ast_exp* scc_cast_expression(t_ast_exp* exp)
 	return exp;
 }
 
-static t_ast_exp* multiplicative_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_multiplicative_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* additive_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_additive_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp*shift_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_shift_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* relational_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_relational_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* equality_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_equality_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* and_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_and_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* exclusive_or_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_exclusive_or_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* inclusive_or_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_inclusive_or_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* logical_and_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_logical_and_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* logical_or_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_logical_or_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* conditional_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_conditional_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-static t_ast_exp* assignment_expression(t_ast_exp* exp)
+static t_ast_exp* ssc_assignment_expression(t_ast_exp* exp)
 {
 	return exp;
 }
 
-t_ast_exp* ssc_expression(t_ast_exp* exp)
+static t_ast_exp* scc_comma_expression(t_ast_exp* exp)
 {
 	return exp;
+}
+
+static t_ast_exp* scc_binary_expression(t_ast_exp* exp)
+{
+	t_ast_exp* r = NULL;
+	
+	assert(exp && exp->kind == AST_EXP_BINARY_KIND);
+
+	switch (exp->u.ast_binary_exp.op)
+	{
+	case AST_OP_ADD :
+	case AST_OP_SUB :
+		{
+			r = ssc_additive_expression(exp);
+			break;
+		}
+	case AST_OP_MUL :
+	case AST_OP_DIV :
+	case AST_OP_MOD :
+		{
+			r = ssc_multiplicative_expression(exp);
+			break;
+		}
+	}
+
+	
+/*
+	AST_OP_BIT_AND,
+	AST_OP_BIT_OR,
+	AST_OP_BIT_XOR,
+	AST_OP_LSHIFT,
+	AST_OP_RSHIFT,
+
+
+	AST_OP_LESS,
+	AST_OP_LESS_EQ,
+	AST_OP_GREAT,
+	AST_OP_GREAT_EQ,
+	AST_OP_EQUAL,
+	AST_OP_UNEQUAL,
+
+	AST_OP_AND,
+	AST_OP_OR,
+
+
+    AST_OP_ASSIGN,
+    AST_OP_MUL_ASSIGN,
+    AST_OP_DIV_ASSIGN,
+    AST_OP_MOD_ASSIGN,
+    AST_OP_ADD_ASSIGN,
+    AST_OP_SUB_ASSIGN,
+    AST_OP_LSHIFT_ASSIGN,
+    AST_OP_RSHIFT_ASSIGN,
+    AST_OP_BIT_AND_ASSIGN,
+    AST_OP_BIT_OR_ASSIGN,
+    AST_OP_BIT_XOR_ASSIGN,
+
+
+	AST_OP_PTR,
+	AST_OP_DOT,
+	AST_OP_INC,
+	AST_OP_DEC
+*/
+	return r;
+}
+
+static t_ast_exp* ssc_expression(t_ast_exp* exp)
+{
+	t_ast_exp* r = NULL;
+
+	assert(exp);
+
+	switch (exp->kind)
+	{
+	case AST_EXP_BINARY_KIND :
+		{
+			r = scc_binary_expression(exp);
+			break;
+		}
+	case AST_EXP_UNARY_KIND :
+		{
+			r = scc_unary_expression(exp);
+			break;
+		}
+	case AST_EXP_IDENTIFIER_KIND :
+		{
+			r = scc_primary_expression(exp);
+			break;
+		}
+	case AST_EXP_FUNCTION_CALL_KIND :
+	case AST_EXP_SUBSCRIPT_KIND :
+	case AST_EXP_INDIR_KIND :
+	case AST_EXP_POSTOP_KIND :
+		{
+			r = scc_postfix_expression(exp);
+			break;
+		}
+	case AST_EXP_CAST_KIND :
+	case AST_EXP_SIZEOF_KIND :
+		{
+			r = scc_unary_expression(exp);
+			break;
+		}
+	case AST_EXP_CONDITION_KIND :
+		{
+			r = ssc_conditional_expression(exp);
+			break;
+		}
+	case AST_EXP_ASSIGNMENT_KIND : 
+		{
+			r = ssc_assignment_expression(exp);
+			break;
+		}
+	case AST_EXP_COMMA_KIND :
+		{
+			r = scc_comma_expression(exp);
+			break;
+		}
+	case AST_EXP_CONST_FLOAT_KIND :
+	case AST_EXP_CONST_DOUBLE_KIND :
+	case AST_EXP_CONST_LONG_DOUBLE_KIND :
+	case AST_EXP_CONST_INTEGER_KIND :
+	case AST_EXP_CONST_LONG_INTEGER_KIND :
+	case AST_EXP_CONST_LONG_LONG_KIND :
+	case AST_EXP_CONST_UNSIGNED_INTEGER_KIND :
+	case AST_EXP_CONST_UNSIGNED_LONG_INTEGER_KIND :
+	case AST_EXP_CONST_UNSIGNED_LONG_LONG_KIND : 
+	case AST_EXP_LITERAL_STRING_KIND :
+	case AST_EXP_LITERAL_STRING_WIDE_KIND :
+		{
+			r = scc_primary_expression(exp);
+			break;
+		}
+	default:
+		assert(0);
+	}
+
+	return r;
 }
